4f385070ba9cecb01cb17ce1e050553dcd884a32
Adham Ali <30821115+adhamali450@users.noreply.github.com>
2026-01-19 06:36:27 +0200

    🧑‍💻 Conditional debug mode by an ENV var

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from src.app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    is_dev = os.getenv("ENV") == "dev"
+    app.run(host="0.0.0.0", port=5000, debug=is_dev)
